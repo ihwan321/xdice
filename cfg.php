7b724043c7e1fccd7ecc0212ac28b0e04e105c94
ihwan321 <63368199+ihwan321@users.noreply.github.com>
2020-04-29 16:04:31 +0700

    Add files via upload

---
+++ cfg.php
@@ -0,0 +1,16 @@
+<?php
+
+//masukan sesuai dibawah
+$cookie4 = "cookie: __cfduid=de8df512fd2197dc2f8724b26fa9a0bc71587579277";
+$cookie5 = "cookie: ExpressCrypto=01bd2d9b0a370c21db4e1ad6f6240f67";
+$cookie6 = "cookie: _ga=GA1.1.604831080.1587579290";
+$cookie8 = "cookie: __gads=ID=168330ed91cea47e:T=1587579341:S=ALNI_MZJ5Iaib0srAimr-uAd0wCKM71t_w";
+$cookie9 = "cookie: _ga_8ES1N2JB0Q=GS1.1.1588150215.48.1.1588150219.0";
+$clientseedbtc = "pgzij1shrvwyxbgvbmt";
+$clientseedltc = "ycp7yulvyk8hh4cxopm";
+$clientseedoge = "86vmm290nehh8zvdypi";
+
+
+?>
+
+
